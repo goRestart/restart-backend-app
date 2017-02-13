6be1fbde8de1c782163f257d85d31c885267309d
Oscar Duran <skyweb09@hotmail.es>
2017-02-13 21:12:45 +0100

    Define basic set of possible user errors

--- Sources/Restart/Domain/Model/User/UserRepositoryProtocol.swift
+++ Sources/Restart/Domain/Model/User/UserRepositoryProtocol.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+public enum AddUserError: Error {
+    case userNameIsAlreadyInUse
+    case emailIsAlreadyInUse
+    case invalidEmail
+    case unknown
+}
+
 public protocol UserRepositoryProtocol {
     func add(with request: AddUserRequest) throws -> User
 }
