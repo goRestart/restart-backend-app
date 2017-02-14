6eec87e34ac8bf2383eab7a388342ee55c89e6f8
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2017-02-14 13:25:53 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
         .Package(url: "https://github.com/goRestart/restart-service-locator.git", majorVersion: 0)
     ],
     exclude: [
