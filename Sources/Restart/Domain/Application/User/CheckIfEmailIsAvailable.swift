e9b6300310aaf3a39afc63e32ae358d3b7c6808e
Oscar Duran <skyweb09@hotmail.es>
2017-02-24 20:58:26 +0100

    Add use case for checking if email is available

---
+++ Sources/Restart/Domain/Application/User/CheckIfEmailIsAvailable.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public struct CheckIfEmailNameIsAvailable {
+
+    private let userService: UserService
+
+    public init(userService: UserService) {
+        self.userService = userService
+    }
+
+    func isAvailable(_ email: String) -> Bool {
+        do {
+            try userService.verify(.email(email))
+        } catch {
+            return false
+        }
+        return true
+    }
+}
