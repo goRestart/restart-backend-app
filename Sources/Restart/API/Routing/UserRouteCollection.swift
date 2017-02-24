aa08357dcd8edc91a368fa9a61e52fae9a039888
Oscar Duran <skyweb09@hotmail.es>
2017-02-24 20:49:49 +0100

    Add route endpoint

--- Sources/Restart/API/Routing/UserRouteCollection.swift
+++ Sources/Restart/API/Routing/UserRouteCollection.swift
@@ -4,6 +4,7 @@ import Routing
 
 private struct Path {
     static let user = "user"
+    static let available = user + "/available"
 }
 
 public struct UserRouteCollection: RouteCollection {
@@ -19,6 +20,10 @@ public struct UserRouteCollection: RouteCollection {
     public func build<Builder: RouteBuilder>(_ builder: Builder) where Builder.Value == Wrapped {
         builder.post(Path.user) { request in
             return try self.userController.post(request)
+        }
+
+        builder.get(Path.available) { request in
+            return try self.userController.verify(request)
         }
     }
 }
