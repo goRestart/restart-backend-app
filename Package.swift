cf197085babb192e6f399f64a20228a974e80dfe
Oscar Duran <skyweb09@hotmail.es>
2017-03-05 20:42:27 +0100

    Add redis provider

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", Version(1, 5, 7)),
         .Package(url: "https://github.com/vapor/mysql-provider.git", Version(1, 1, 1)),
+        .Package(url: "https://github.com/vapor/redis-provider.git", Version(1, 0, 1)),
         .Package(url: "https://github.com/goRestart/restart-service-locator.git", majorVersion: 0)
     ],
     exclude: [
