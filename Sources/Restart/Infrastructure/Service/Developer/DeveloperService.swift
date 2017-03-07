72f5cd7eaf3a4c28ddd46919b50acb3836b4f1b2
Oscar Duran <skyweb09@hotmail.es>
2017-03-07 19:13:10 +0100

    Connect the task with the developer service

--- Sources/Restart/Infrastructure/Service/Developer/DeveloperService.swift
+++ Sources/Restart/Infrastructure/Service/Developer/DeveloperService.swift
@@ -3,12 +3,20 @@ import Foundation
 public struct DeveloperService {
 
     private let checkIfApiKeyIsValidTask: CheckIfApiKeyIsValidTask
+    private let getPrivateKeyTask: GetPrivateKeyTask
 
-    init(checkIfApiKeyIsValidTask: CheckIfApiKeyIsValidTask) {
+    init(checkIfApiKeyIsValidTask: CheckIfApiKeyIsValidTask,
+         getPrivateKeyTask: GetPrivateKeyTask)
+    {
         self.checkIfApiKeyIsValidTask = checkIfApiKeyIsValidTask
+        self.getPrivateKeyTask = getPrivateKeyTask
     }
 
     func isApiKeyValid(privateKey: String, publicKey: String) throws {
         return try checkIfApiKeyIsValidTask.execute(privateKey, publicKey)
+    }
+
+    func getPrivateKey(with publicKey: String) throws -> String {
+        return try getPrivateKeyTask.execute(publicKey)
     }
 }
