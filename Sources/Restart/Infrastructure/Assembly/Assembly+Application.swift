56cc92deaec9a18a795986f5d28f006e51a8b291
Oscar Duran Grillo <skyweb09@hotmail.es>
2017-04-05 13:34:37 +0200

    Droplet can fail when initializing

--- Sources/Restart/Infrastructure/Assembly/Assembly+Application.swift
+++ Sources/Restart/Infrastructure/Assembly/Assembly+Application.swift
@@ -6,7 +6,7 @@ extension Assembly {
     func getDroplet() -> Droplet {
         let key = String(describing: Droplet.self)
         guard let droplet: Droplet = Resolve(key) else {
-            let service: Droplet = Droplet()
+            let service: Droplet = try! Droplet()
             return Register(service, key: key)
         }
         return droplet
