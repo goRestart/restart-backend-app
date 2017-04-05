1a2a9ae68cec33e17e6ce4fc6639e742fe9326d8
Oscar Duran Grillo <skyweb09@hotmail.es>
2017-04-05 13:33:45 +0200

    Remove suggestion entity

--- Sources/Restart/Domain/Model/Suggestion/SuggestionRepositoryProtocol.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public protocol SuggestionRepositoryProtocol {
-    func add(suggestion: String, platform: Platform) throws -> Suggestion
-    func get(with query: String) -> [Suggestion]
-}
