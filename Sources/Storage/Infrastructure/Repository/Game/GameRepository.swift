340006f85f39673873d4b6903fec192915577c55
Oscar Duran Grillo <skyweb09@hotmail.es>
2017-05-10 21:22:33 +0200

    Add disk datasource dependency to the game repository

--- Sources/Storage/Infrastructure/Repository/Game/GameRepository.swift
+++ Sources/Storage/Infrastructure/Repository/Game/GameRepository.swift
@@ -3,7 +3,13 @@ import Domain
 
 public struct GameRepository: GameRepositoryProtocol {
     
+    private let diskDataSource: GameDataSource
+    
+    init(diskDataSource: GameDataSource) {
+        self.diskDataSource = diskDataSource
+    }
+    
     public func getAllPlatforms() throws -> [Platform] {
-        return []
+        return try diskDataSource.getAllPlatforms()
     }
 }
