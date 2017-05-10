56a6d73018bd304e43656030a7aa053ff3a6d72d
Oscar Duran Grillo <skyweb09@hotmail.es>
2017-05-10 20:48:05 +0200

    Add game genre seeder

--- Sources/FluentStorage/Seeds/Game Genre/GameGenreSeeder.swift
+++ Sources/FluentStorage/Seeds/Game Genre/GameGenreSeeder.swift
@@ -2,33 +2,45 @@ import Fluent
 
 final class GameGenreSeeder: Seeder {
     
-    var description: String?
+    var description: String? = "Seed db with some test game genres"
     
     static func prepare(_ database: Database) throws {
         
         let englishLocale = LocaleDiskModel(
             localeIdentifier: "en_US"
         )
-        
+
+        let spanishLocale = LocaleDiskModel(
+            localeIdentifier: "es_ES"
+        )
+
         try englishLocale.save()
+        try spanishLocale.save()
         
         let testGenres = [
-            "Fighting", "Graphic Adventure", "Platform", "Vehicle Simulator", "Virtual World", "Shooter"
+            (en: "Fighting", es: "Pelea"),
+            (en: "Platform", es: "Plataforma"),
+            (en: "Shooter", es: "Disparos")
         ]
         
         try testGenres.forEach { gameGenreName in
+
+            let gameGenre = GameGenreDiskModel()
+            try gameGenre.save()
             
-            let translation = TranslationDiskModel<GameGenreDiskModel>(
-                value: gameGenreName,
+            let englishTranslation = TranslationDiskModel<GameGenreDiskModel>(
+                value: gameGenreName.en,
+                parentId: gameGenre.id,
                 localeId: englishLocale.id
             )
-            try translation.save()
+            try englishTranslation.save()
             
-            let gameGenre = GameGenreDiskModel(
-                translationId: translation.id
+            let spanishTranslation = TranslationDiskModel<GameGenreDiskModel>(
+                value: gameGenreName.es,
+                parentId: gameGenre.id,
+                localeId: spanishLocale.id
             )
-            
-            try gameGenre.save()
+            try spanishTranslation.save()
         }
     }
     
