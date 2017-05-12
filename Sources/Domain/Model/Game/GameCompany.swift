8f2085b570585793f9fa3b9a9fbef1e219625731
Oscar Duran Grillo <skyweb09@hotmail.es>
2017-05-12 02:07:43 +0200

    Add identifier to game company

--- Sources/Domain/Model/Game/GameCompany.swift
+++ Sources/Domain/Model/Game/GameCompany.swift
@@ -2,10 +2,15 @@ import Foundation
 
 public struct GameCompany {
     
+    public let identifier: String
     public let name: String
     public let description: String?
     
-    public init(name: String, description: String?) {
+    public init(identifier: String,
+                name: String,
+                description: String?)
+    {
+        self.identifier = identifier
         self.name = name
         self.description = description
     }
